b782c149ee2aaff2c73911f697bb30a1b593be99
q133ss <info@promo-kick.ru>
2022-03-14 20:44:32 +0300

    subscribes

--- app/Http/Controllers/Front/PriceController.php
+++ app/Http/Controllers/Front/PriceController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Subscribe;
 
 class PriceController extends Controller
 {
     public function index(){
-        return view('price');
+        $subscribes = Subscribe::get();
+        return view('price', compact('subscribes'));
     }
 }
--- resources/views/price.blade.php
+++ resources/views/price.blade.php
@@ -6,96 +6,26 @@
             <h1 class="title prices__title">Стоимость</h1>
             <p class="subtitle prices__subtitle">Варианты и стоимость абонементов в нашем зале</p>
             <ul class="prices__list">
+                @foreach($subscribes as $sub)
                 <li class="prices__item">
                     <div class="item-prices">
                         <div class="item-prices__logo">
                             <img src="/assets/img/logo.svg" alt="Everest" aria-hidden="true">
                         </div>
-                        <p class="item-prices__title">Тренажёрный зал</p>
+                        <p class="item-prices__title">{{$sub->title}}</p>
                         <ul class="item-prices__list">
-                            <li class="item-prices__point"> Абонемент "ГОД": <span>17 000 ₽</span>
+                            @foreach(json_decode($sub->price) as $title => $price)
+                            <li class="item-prices__point"> {{$title}} <span>{{$price}} ₽</span>
                             </li>
-                            <li class="item-prices__point"> Абонемент "ПОЛГОДА": <span>10 000 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Абонемент "3 МЕСЯЦА": <span>8 000 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Абонемент "1 МЕСЯЦ": <span>4 000 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Абонемент "ГОД утренний": <span>12 000 ₽</span>
-                            </li>
+                            @endforeach
                         </ul>
-						<p class="item-prices__scriptum">Все абонементы включают:
-							<br/>- 1 тренировка кроссфит
-							<br/>- 1 персональная тренировка в тренажерном зале
-							<br/>- 1 персональная тренировка в зале единоборств
-							<br/>- посещение турецкой парной
-							<br/>- посещение зала единоборств (вне групповых тренировок)
+						<p class="item-prices__scriptum">
+                            {{$sub->description}}
 						</p>
-                        <button class="button item-prices__btn" onclick="$('#order_type').val('Абонимент: Тренажёрный зал')" data-open-modal="request">Записаться</button>
+                        <button class="button item-prices__btn" onclick="$('#order_type').val('Абонимент: {{$sub->title}}')" data-open-modal="request">Записаться</button>
                     </div>
                 </li>
-                <li class="prices__item">
-                    <div class="item-prices">
-                        <div class="item-prices__logo">
-                            <img src="/assets/img/logo.svg" alt="Everest" aria-hidden="true">
-                        </div>
-                        <p class="item-prices__title">Секции "Взрослые"</p>
-                        <ul class="item-prices__list">
-                            <li class="item-prices__point"> Пакет "8 ЗАНЯТИЙ": <span>3 500 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Пакет "12 ЗАНЯТИЙ": <span>4 500 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Пакет "16 ЗАНЯТИЙ": <span>5 500 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Пакет "20 ЗАНЯТИЙ": <span>6 500 ₽</span>
-                            </li>
-                        </ul>
-						<p class="item-prices__scriptum">Каждый пакет включает:
-							<br/>- посещение секций в зале единоборств
-							<br/>- посещение секций в тренажерном зале
-							<br/>- посещение тренажерного зала с 12 до 16 часов ежедневно
-							<br/>- посещение турецкой парной
-						</p>
-                        <button class="button item-prices__btn" onclick="$('#order_type').val('Абонимент: Зал единоборств')" data-open-modal="request">Записаться</button>
-                    </div>
-                </li>
-                <li class="prices__item">
-                    <div class="item-prices">
-                        <div class="item-prices__logo">
-                            <img src="/assets/img/logo.svg" alt="Everest" aria-hidden="true">
-                        </div>
-                        <p class="item-prices__title">Секции единоборств "Дети"</p>
-                        <ul class="item-prices__list">
-                            <li class="item-prices__point"> Пакет "8 ЗАНЯТИЙ": <span>3 500 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Пакет "12 ЗАНЯТИЙ": <span>4 000 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Пакет "16 ЗАНЯТИЙ": <span>5 000 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Пакет "20 ЗАНЯТИЙ": <span>6 000 ₽</span>
-                            </li>
-                        </ul>
-                        <button class="button item-prices__btn" onclick="$('#order_type').val('Абонимент: Индивидуальные тренеровки')" data-open-modal="request">Записаться</button>
-                    </div>
-                </li>
-                <li class="prices__item">
-                    <div class="item-prices">
-                        <div class="item-prices__logo">
-                            <img src="/assets/img/logo.svg" alt="Everest" aria-hidden="true">
-                        </div>
-                        <p class="item-prices__title">Персональная тренировка</p>
-                        <ul class="item-prices__list">
-                            <li class="item-prices__point"> Разовая тренировка: <span>от 1 800 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Пакет "5 ТРЕНИРОВОК": <span>от 8 000 ₽</span>
-                            </li>
-                            <li class="item-prices__point"> Пакет "10 ТРЕНИРОВОК": <span>от 15 000 ₽</span>
-                            </li>
-                        </ul>
-                        <p class="item-prices__scriptum">*Цены зависят от выбранного Вами тренера</p>
-                        <button class="button item-prices__btn" onclick="$('#order_type').val('Абонимент: Индивидуальные тренеровки')" data-open-modal="request">Записаться</button>
-                    </div>
-                </li>
+                @endforeach
             </ul>
 			<div class="about__text" style="margin-top: 50px;">
 				<p>Информация, представленная на сайте, не является публичной офертой</p>
